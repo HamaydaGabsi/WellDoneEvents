58f94bce4b6380eae8048974c0a8c4b43d781a28
HamaydaGabsi <hamayda.gabsi@insat.ucar.tn>
2022-11-18 18:11:13 +0100

    resized navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -11,7 +11,8 @@ const Navbar = () =>{
             <div className='nav-row-1'>
                 <a className='nav-title acceuil ' href='#home'>Acceuil</a>
                 <a className=' nav-title services ' href='#home'>Services</a></div>
-                <div className='h-100 image-container '>
+                {/* add bg-dark,image-container and remove h-100  */}
+                <div className='h-100  '> 
                     <img id='logo' className='position-relative nav-row-2' src={logo} alt="logo" />
                 </div>
                 <div className='nav-row-3'>
