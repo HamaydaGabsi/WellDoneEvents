730851491ea1fb763de5e14d413c188b3eab958d
walid elbir <walid.el.biir@gmail.com>
2022-11-19 15:51:25 +0000

    fetching carousel photos done

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,38 +1,63 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import Carousel from 'react-bootstrap/Carousel';
-import photo from './DSC_0304.jpg';
-import photo1 from './IMG_0006.jpg';
-import photo2 from './IMG_0621.jpg';
 import './Carousel.css'
+
+
 
 
 const PhotoCarousel = () =>{
 
+  const [carousels , setcarousels] = useState([])
+  const fetch_carousel = () =>{
+    let tab =[];
+    fetch('http://localhost:5000/api/carousel/active/carousels')
+    .then(response => response.json())
+    .then((data) =>{ 
+      data.data.forEach((carousel) =>{
+          const activecarousel = {
+            id : carousel._id,
+            image : carousel.image
+          }
+          tab.push(activecarousel);
+          }
+        )
+        setcarousels(tab)
+        console.log(carousels)
+      }
+    )
+    .catch(err => console.log(err));
+  }
+
+
+  const add_carousel_item = () =>{
+    const carousel = document.querySelector('.carousel-container')
+    for(let i = 0; i <carousels.length; i++) {
+      const carousel_item = 
+       <Carousel.Item  className={`carousel-item${i}`}>
+      <img
+        className="d-block w-100 h-100"
+        src={carousels[i].image}
+        alt="First slide"
+      />
+    </Carousel.Item> 
+      carousel.children[i].appendChild(carousel_item)
+    }
+  }
+  
+  useEffect(()=>{
+    fetch_carousel()
+    // add_carousel_item()
+  } , [carousels.length])
+
     return(
       <div className=" carousel-container">
           <Carousel  fade>
-            <Carousel.Item>
+            <Carousel.Item  className="carousel-item-1">
               <img
                 className="d-block w-100 h-100"
-                src={photo}
+                src={carousels[0].image}
                 alt="First slide"
               />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100 h-100"
-                src={photo1}
-                alt="Second slide"
-              />
-
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100 h-100"
-                src={photo2}
-                alt="Third slide"
-              />
-
             </Carousel.Item>
           </Carousel>
       </div>
--- src/Pages/Accueil.js
+++ src/Pages/Accueil.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PhotoCarousel from '../Components/Carousel/Carousel';
-
 import PostsContainer from '../Components/PostsContainer/PostsContainer'; 
 
 
+
 const Accueil = () => {
+
+
     return (
   <>
     <PhotoCarousel />
