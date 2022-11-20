8a651b6b494783092e01972c9e834589cb03eccf
HamaydaGabsi <hamayda.gabsi@insat.ucar.tn>
2022-11-20 13:06:39 +0100

    resolved undetected conflicts

--- src/Components/HomePost/HomePost.js
+++ src/Components/HomePost/HomePost.js
@@ -12,7 +12,7 @@ const check_parity =(a) =>{
 const HomePost = ({order}) =>{
 
   return (
-    <div className ={`post-container post-container-${check_parity(order)} d-flex w-100 mb-3`}>
+    <div className ={`post-container post-container-${check_parity(order)} d-flex w-100 `}>
         <div className='d-flex h-100 post-caption'>
             <div className='caption-title title'> TITLE ! </div>
             <div className='caption-p commontext'> 
--- src/Components/PostsContainer/PostsContainer.js
+++ src/Components/PostsContainer/PostsContainer.js
@@ -5,7 +5,7 @@ import HomePost from "../HomePost/HomePost";
 const PostsContainer = () =>{
 
     return (
-        <div className=" my-5 mx-5 d-flex cardscontainer">
+        <div className="d-flex cardscontainer">
             <HomePost order={0} />
             <HomePost order={1} />
             <HomePost order={2} />
