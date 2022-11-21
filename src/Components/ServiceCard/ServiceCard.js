069d85502877db903f87f1d21d30cf1e63ce3dca
HamaydaGabsi <hamayda.gabsi@insat.ucar.tn>
2022-11-21 10:22:58 +0100

    made services responsive

--- src/Components/ServiceCard/ServiceCard.js
+++ src/Components/ServiceCard/ServiceCard.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import './ServiceCard.css'
+import React from "react";
+import "./ServiceCard.css";
 
-
-
-const ServiceCard = ({card}) =>{
-
-    return(
-        <div className='card-container '>
-            <div className=' shadow-cover  d-flex'>
-                <div className='cardtitle title'>
-                    {card.title}
-                </div>
-            </div>
-            <div className='bg-image'>
-                    <img  src={card.image} alt="" />
-                </div>
-        </div>
-    )
- 
-}
+const ServiceCard = ({ card }) => {
+  return (
+    <div className="card-container ">
+      <div className=" shadow-cover  d-flex">
+        <div className="cardtitle title">{card.title}</div>
+      </div>
+      <div className="bg-image">
+        <img src={card.image} alt="" />
+      </div>
+    </div>
+  );
+};
 
 export default ServiceCard;
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,74 +1,87 @@
-import React, { useEffect, useState }  from 'react';
-import './Services.css'
-import Cardsrow from '../../Components/Cardsrow/Cardsrow.js';
-import test_image from '../../Components/HomePost/IMG_9745.jpg';
+import React, { useEffect, useState } from "react";
+import "./Services.css";
+import Cardsrow from "../../Components/Cardsrow/Cardsrow.js";
+import test_image from "../../Components/HomePost/IMG_9745.jpg";
 
 const Services = () => {
-
-    const cover_animation = () => {
-        const cardcontainers = document.querySelectorAll('.card-container')
-        cardcontainers.forEach((container) =>{
-            const shadow_cover = container.children[0]
-            const card_title = shadow_cover.children[0]
-            container.addEventListener('mouseenter', () => {
-                shadow_cover.classList.add('shadow-cover-visible')
-                shadow_cover.classList.add('animate__fadeIn')
-                shadow_cover.classList.add('animate__animated')
-                card_title.classList.add('animate__animated')
-                card_title.classList.add('animate__zoomIn')
-            })
-            container.addEventListener('mouseleave', () =>{
-                shadow_cover.classList.remove('shadow-cover-visible')
-                shadow_cover.classList.remove('animate__animated')
-                shadow_cover.classList.remove('animate__fadeIn')
-                card_title.classList.remove('animate__animated')
-                card_title.classList.remove('animate__zoomIn')
-            }
-            )
-        })
+  //Responsiveness
+  const [nbCards, setNbCards] = useState(3);
+  const ajustCardsNumber = function() {
+    if (window.innerWidth <= 1024 && window.innerWidth > 768) {
+     setNbCards(2)
+    } else if(window.innerWidth <= 768){
+        setNbCards(1);
     }
-    
+    else setNbCards(3);
+  };
+  useEffect(ajustCardsNumber)
+  window.onresize = ajustCardsNumber;
 
-    const general_card = {
-        title : 'TITLE',
-        image : test_image
-    }
-    const [cards , setcards] = useState([general_card,general_card,general_card,general_card])
+  const cover_animation = () => {
+    const cardcontainers = document.querySelectorAll(".card-container");
+    cardcontainers.forEach((container) => {
+      const shadow_cover = container.children[0];
+      const card_title = shadow_cover.children[0];
+      container.addEventListener("mouseenter", () => {
+        shadow_cover.classList.add("shadow-cover-visible");
+        shadow_cover.classList.add("animate__fadeIn");
+        shadow_cover.classList.add("animate__animated");
+        card_title.classList.add("animate__animated");
+        card_title.classList.add("animate__zoomIn");
+      });
+      container.addEventListener("mouseleave", () => {
+        shadow_cover.classList.remove("shadow-cover-visible");
+        shadow_cover.classList.remove("animate__animated");
+        shadow_cover.classList.remove("animate__fadeIn");
+        card_title.classList.remove("animate__animated");
+        card_title.classList.remove("animate__zoomIn");
+      });
+    });
+  };
 
-    const fetch_cards=() => {
-        fetch('/api/services/active/post')
-        .then(response => response.json())
-        .then(data => {
-            if(data.data.length !== 0)
-            setcards(data.data)
-        })
-    }
+  const general_card = {
+    title: "TITLE",
+    image: test_image,
+  };
+  const [cards, setcards] = useState([
+    general_card,
+    general_card,
+    general_card,
+    general_card,
+  ]);
 
-    useEffect(fetch_cards , [])
-    useEffect(cover_animation)
+  const fetch_cards = () => {
+    fetch("/api/services/active/post")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.data.length !== 0) setcards(data.data);
+      });
+  };
 
-    const cards_row_array = []
-    const slice_cards = (cards , row_length) =>{
-        const cards_copy = [...cards]
-        while(cards_copy.length>0)
-        {let chunk = cards_copy.splice(0,row_length)
-        cards_row_array.push(chunk)}
-    }
+  useEffect(fetch_cards, []);
+  useEffect(cover_animation);
 
-    const create_cardsRow = (row_cards) => {
-        return(
-            <Cardsrow cards = {row_cards} />
-        )
+  const cards_row_array = [];
+  const slice_cards = (cards, row_length) => {
+    const cards_copy = [...cards];
+    while (cards_copy.length > 0) {
+      let chunk = cards_copy.splice(0, row_length);
+      cards_row_array.push(chunk);
     }
-    const generate_card_container  = (cards) => {
-        slice_cards(cards,2)
-        return cards_row_array.map(create_cardsRow)
-    }
-    return (
-        <div className='CardsContainer d-flex '>
-           {generate_card_container(cards)}
-        </div>
-    )
-}
+  };
+
+  const create_cardsRow = (row_cards) => {
+    return <Cardsrow cards={row_cards} />;
+  };
+  const generate_card_container = (cards) => {
+    slice_cards(cards, nbCards);
+    return cards_row_array.map(create_cardsRow);
+  };
+  return (
+    <div className="CardsContainer d-flex ">
+      {generate_card_container(cards)}
+    </div>
+  );
+};
 
 export default Services;
