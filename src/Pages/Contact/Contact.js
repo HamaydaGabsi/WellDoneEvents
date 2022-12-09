2b7e1e0e770e6a12d14027508a7acb44cf8d8ab5
walid elbir <walid.el.biir@gmail.com>
2022-12-09 14:03:52 +0000

    fixed contact info

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -24,10 +24,10 @@ const Contact = () =>{
             <ContactForm />
             <div className="info me-5 w-50  d-flex">
                 <ul className="my-auto info-list  commontext mx-auto">
-                    <li>Numéro de téléphone: {socials.tel}</li>
-                    <li>Email: {socials.email}</li>
-                    <li>Adresse: {socials.adress}</li>
-                    <li>Page facebook: {socials.facebook}</li>
+                    <li>Numéro de téléphone: <span className="info-text"> {socials.tel}</span></li>
+                    <li>Email:<span className="info-text"> {socials.email}</span></li>
+                    <li>Adresse:<span className="info-text"> {socials.adress}</span></li>
+                    <li>Page facebook: <a id="welldone-facebook" target='_blank' href={socials.facebook}> Well Done Events</a> </li>
                 </ul>
             </div>
             
